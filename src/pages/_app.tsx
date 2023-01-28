7f9b785277199b1786334d4114820f3ee215dab0
sirzes02 <sirzes02@gmail.com>
2023-01-28 15:45:07 -0500

    add bootstrap and font-awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/bootstrap.min.css";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossOrigin="anonymous"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
